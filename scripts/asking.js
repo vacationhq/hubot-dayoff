8fe0b53a2b1c56bd2458eb6310bfef5e12d0fea5
Mosky <mosky.tw@gmail.com>
2016-05-15 17:30:04 +0800

    Make the resp more cleaner

--- scripts/asking.js
+++ scripts/asking.js
@@ -9,7 +9,7 @@ module.exports = function(robot) {
 
     robot.respond(/.*剩.+假.*/, function(res) {
 
-        res.reply('等等我我找一下⋯⋯');
+        res.reply('等等，我找一下⋯⋯');
 
         async.series([
             function setAuth(step) {
